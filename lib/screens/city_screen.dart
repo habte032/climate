048f069791892a1045d8d97c05b12cfd3ef20483
habte032 <habte032@gmail.com>
2023-07-30 14:40:24 +0300

    fix:Make scrollable

--- lib/screens/city_screen.dart
+++ lib/screens/city_screen.dart
@@ -12,81 +12,83 @@ class _CityScreenState extends State<CityScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('images/city_background.png'),
-            fit: BoxFit.fill,
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('images/city_background.png'),
+              fit: BoxFit.fill,
+            ),
           ),
-        ),
-        constraints: BoxConstraints.expand(),
-        child: SafeArea(
-          child: Column(
-            children: <Widget>[
-              Align(
-                alignment: Alignment.topLeft,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  child: Icon(
-                    Icons.arrow_back_ios,
-                    color: Colors.black,
-                    size: 30.0,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 100,
-              ),
-              Container(
-                padding: EdgeInsets.all(20.0),
-                child: TextField(
-                  onChanged: (value){
-                    cityName=value;
-                  },
-                  onSubmitted: (value){
-                    cityName=value;
-                    Navigator.pop(context,cityName);
-                  },
-                  style: TextStyle(
-                      color: Colors.black
-                  ),
-                  decoration: InputDecoration(
-                      fillColor: Colors.white,
-                      filled: true,
-                      prefixIcon: Icon(Icons.location_city_rounded,),
-                      prefixIconColor: Colors.black,
-                      hintText: 'Enter City' ,
-                      hintStyle: TextStyle(
-                        color: Colors.black,
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30),
-
-                      )
-                  ),
-                ),
-              ),
-              Center(
-                child: Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
-                  decoration:  BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Color(0xffd1ac9a),
-                  ),
+          constraints: BoxConstraints.expand(),
+          child: SafeArea(
+            child: Column(
+              children: <Widget>[
+                Align(
+                  alignment: Alignment.topLeft,
                   child: TextButton(
                     onPressed: () {
-                      Navigator.pop(context,cityName);
+                      Navigator.pop(context);
                     },
-                    child: Text(
-                      'Get Weather',
-                      style: kButtonTextStyle,
+                    child: Icon(
+                      Icons.arrow_back_ios,
+                      color: Colors.black,
+                      size: 30.0,
                     ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(
+                  height: 100,
+                ),
+                Container(
+                  padding: EdgeInsets.all(20.0),
+                  child: TextField(
+                    onChanged: (value){
+                      cityName=value;
+                    },
+                    onSubmitted: (value){
+                      cityName=value;
+                      Navigator.pop(context,cityName);
+                    },
+                    style: TextStyle(
+                        color: Colors.black
+                    ),
+                    decoration: InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        prefixIcon: Icon(Icons.location_city_rounded,),
+                        prefixIconColor: Colors.black,
+                        hintText: 'Enter City' ,
+                        hintStyle: TextStyle(
+                          color: Colors.black,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+
+                        )
+                    ),
+                  ),
+                ),
+                Center(
+                  child: Container(
+                    padding: EdgeInsets.symmetric(horizontal: 20,vertical: 5),
+                    decoration:  BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Color(0xffd1ac9a),
+                    ),
+                    child: TextButton(
+                      onPressed: () {
+                        Navigator.pop(context,cityName);
+                      },
+                      child: Text(
+                        'Get Weather',
+                        style: kButtonTextStyle,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
